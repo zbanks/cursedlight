ddffb5b378d22e33cdfc7dae80bd7905c4a513ad
Zach Banks <zbanks@mit.edu>
2013-08-19 18:57:44 -0400

    Broken, but starting to switch to urwid

--- cl.py
+++ cl.py
@@ -1,27 +1,33 @@
 import Queue
 import asyncore
-import curses
 import evdev
 import logging
 import serial
 import threading
 import time
+import glob
+import urwid
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
-kbd_evs = Queue.Queue()
-
 class CanBus(object):
     def __init__(self, port, baudrate=115200):
         self.ser = serial.Serial(port, baudrate)
+        self.addresses = {}
 
     def raw_packet(data):
         self.ser.write("".join([chr(d) for d in data]))
 
     def can_packet(addr, can_data):
+        if addr not in self.addresses:
+            self.addresses[addr] = "(Unknown)"
         data = [(addr >> 8) & 0xff, addr & 0xff, 8] + (can_data + [0] * 8)[:8]
         self.raw_packet(data)
+
+    def send_to_all(can_data):
+        for addr in self.addresses:
+            self.can_packet(addr, can_data)
 
 class FakeCanBus(CanBus):
     def __init__(self, port, baudrate=115200):
@@ -95,21 +101,27 @@ class RawKeyboard(object):
         self.dev = evdev.InputDevice(dev_input)
 
 class AsyncRawKeyboard(threading.Thread):
-    def __init__(self, dev_input, kid, grab=False):
+    def __init__(self, dev_input, kid, kbd_evs, grab=False):
         self.dev = evdev.InputDevice(dev_input)
-        self.kid = kid
-        self.running = True
-        self.grab = grab
-        if self.grab:
-            self.dev.grab()
-        threading.Thread.__init__(self)
+        if evdev.ecodes.EV_KEY in self.dev.capabilities() and evdev.ecodes.EV_LED in self.dev.capabilities():
+            self.is_keyboard = True
+            self.kid = kid
+            self.kbd_evs = kbd_evs
+            self.running = True
+            self.grab = grab
+            if self.grab:
+                self.dev.grab()
+            threading.Thread.__init__(self)
+        else:
+            self.is_keyboard = False
+            self.running = False
 
     def run(self):
         for ev in self.dev.read_loop():
             if not self.running:
                 return
             if ev.type == evdev.ecodes.EV_KEY:
-                kbd_evs.put((self.kid, ev))
+                self.kbd_evs.put((self.kid, ev))
 
     def stop(self):
         self.running = False
@@ -118,19 +130,33 @@ class AsyncRawKeyboard(threading.Thread):
         self.join()
 
 class CursedLightUI(object):
-    def __init__(self, canbus):
+    palette = [
+        ('bpm', '', '', '', '#333', '#ddd'),
+        ('bpm_text', '', '', '', '#333', '#ddd'),
+        ('bg', '', '', '', '#333', '#ddd'),
+    ]
+    def __init__(self, canbus, kbd_evs):
         self.canbus = canbus
+        self.kbd_evs = kbd_evs
         self.tb = Timebase()
         self.running = True
 
-        self.scr = curses.initscr()
-        self.scr.addstr(0, 0, "Yay curses!")
-        self.scr.refresh()
+        placeholder = urwid.SolidFill()
+        self.loop = urwid.MainLoop(placeholder, self.palette)
+        self.loop.screen.set_terminal_properties(colors=256)
+        self.loop.widget = urwid.AttrMap(placeholder, 'bg')
+        self.loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+        self.loop.event_loop.enter_idle(lambda: self.idle_loop())
 
+        self.pile = self.loop.widget.base_widget
 
-    def loop_forever(self):
-        data = []
-        keypress_master = {
+        self.status = urwid.Text(('status', 'CursedLight - Debug'), align='center')
+        self.bpm = urwid.Text("", align='center')
+
+        for item in [self.status, self.bpm]:
+            self.pile.contents.append((item, self.pile.options()))
+
+        self.keypress_master = {
             evdev.ecodes.KEY_D: lambda ev: self.tb.multiply(2),
             evdev.ecodes.KEY_E: lambda ev: self.tb.quantize(),
             evdev.ecodes.KEY_H: lambda ev: self.tb.multiply(0.5),
@@ -139,37 +165,64 @@ class CursedLightUI(object):
             evdev.ecodes.KEY_T: lambda ev: self.tb.tap(ev.timestamp()),
         }
 
-        while self.running:
-#data.append(self.scr.getch())
-            try:
-                kid, ev = kbd_evs.get_nowait()
-            except Queue.Empty:
-                pass
-            else:
-                if kid == 0 and ev.value == 1: #T down
-                    if ev.code in keypress_master:
-                        keypress_master[ev.code](ev)
+    def loop_forever(self):
+        self.loop.run()
 
-            self.scr.addstr(1, 0, "Tick: %d.%03d" % self.tb.tick())
-            self.scr.addstr(2, 0, "BPM: %0.1f" % self.tb.bpm)
-            self.scr.refresh()
+    def idle_loop(self):
+        try:
+            kid, ev = self.kbd_evs.get_nowait()
+        except Queue.Empty:
+            pass
+        else:
+            if kid == 0 and ev.value == 1: #T down
+                if ev.code in self.keypress_master:
+                    self.keypress_master[ev.code](ev)
+
+        self.bpm.set_text([('bpm_text', 'Tick: '), ('bpm', '{0}.{1:03d}'.format(*self.tb.tick())),
+                           ('bpm_text', 'BPM: '), ('bpm', '{: <6.01f}'.format(self.tb.bpm))])
+        """
+        self.scr.addstr(1, 0, "Tick: {0}.{1:03d}".format(*self.tb.tick()))
+        self.scr.addstr(2, 0, "BPM:  {: <6.01f}".format(self.tb.bpm))
+        self.scr.refresh()
+        """
     
     def stop(self):
-        self.running = False
+        raise urwid.ExitMainLoop()
 
     def cleanup(self):
-        curses.endwin()
+        pass
 
 
-if __name__ == "__main__":
-    kbd = AsyncRawKeyboard("/dev/input/event4", 0)
+def setup_keyboards():
+    kbd_evs = Queue.Queue()
+    kbds = []
+    for path in glob.glob("/dev/input/event*"):
+        kbd = AsyncRawKeyboard(path, len(kbds), kbd_evs, grab=False)
+        if not kbd.is_keyboard:
+            continue
+        kbds.append(kbd)
+        kbd.start()
+    return kbd_evs, kbds
+
+def stop_keyboards(kbds):
+    [k.stop() for k in kbds]
+
+def main():
+    kbd_evs, keyboards = setup_keyboards()
+    print "Found %d keyboards" % len(keyboards)
+    time.sleep(0.5)
     bus = FakeCanBus("/dev/ttyUSB0", 115200)
-    kbd.start()
-    ui = CursedLightUI(bus)
+    ui = CursedLightUI(bus, kbd_evs)
     try:
         ui.loop_forever()
     except KeyboardInterrupt:
-        # ui.cleanup()
         pass
+    except Exception:
+        ui.cleanup()
+        stop_keyboards(keyboards)
+        raise
     ui.cleanup()
-    kbd.stop()
+    stop_keyboards(keyboards)
+
+if __name__ == "__main__":
+    main()
